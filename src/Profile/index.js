c60bce591302571d356786372370eceac4165cdb
nate <nathaniel@clicktripz.com>
2019-04-11 20:11:51 -0700

    reset

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -6,41 +6,7 @@ import RepositoryList, { REPOSITORY_FRAGMENT } from '../Repository';
 import Loading from '../Loading';
 import ErrorMessage from '../Error';
 
-const GET_REPOSITORIES_OF_CURRENT_USER = gql `
-  {
-    viewer {
-      repositories(
-        first: 5
-        orderBy: { direction: DESC, field: STARGAZERS }
-      ) {
-        edges {
-          node {
-            ...repository
-          }
-        }
-      }
-    }
-  }
-
-  ${REPOSITORY_FRAGMENT}
-`;
-
 const Profile = () => (
-    <Query query={GET_REPOSITORIES_OF_CURRENT_USER}>
-      {({ data, loading, error }) => {
-        if (error) {
-          return <ErrorMessage error={error} />;
-        }
-
-        const { viewer } = data;
-
-        if (loading || !viewer) {
-          return <Loading />;
-        }
-
-        return <RepositoryList repositories={viewer.repositories} />;
-      }}
-    </Query>
 
 );
 
--- src/Repository/RepositoryList/index.js
+++ src/Repository/RepositoryList/index.js
@@ -4,11 +4,8 @@ import RepositoryItem from '../RepositoryItem';
 
 import '../style.css';
 
-const RepositoryList = ({ repositories }) =>
-  repositories.edges.map(({ node }) => (
-    <div key={node.id} className="RepositoryItem">
-      <RepositoryItem {...node} />
-    </div>
-  ));
+const RepositoryList = ({}) =>
+
+  );
 
 export default RepositoryList;
